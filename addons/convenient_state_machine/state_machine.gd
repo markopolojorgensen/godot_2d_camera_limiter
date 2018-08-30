ba90246bd13abd3f464ebc6b2ea901aa31dd013e
Mark J <marko.polo.jorgensen@gmail.com>
2018-08-29 22:43:18 -0500

    state machine debug option

--- addons/convenient_state_machine/state_machine.gd
+++ addons/convenient_state_machine/state_machine.gd
@@ -1,5 +1,7 @@
 extends Node
 
+export(bool) var debug = false
+
 var current_state setget set_current_state
 
 var transitions = {
@@ -18,23 +20,28 @@ func set_current_state(new_state):
 		return
 	
 	if not transitions.has(current_state):
-		var message = "no transitions out of state %s (to %s or anything), not transitioning"
-		print(message % [str(current_state), str(new_state)])
+		if debug:
+			var message = "no transitions out of state %s (to %s or anything), not transitioning"
+			print(message % [str(current_state), str(new_state)])
 		return
 	
 	if not transitions[current_state].has(new_state):
-		var message = "no transition from %s to %s specified"
-		print(message % [str(current_state), str(new_state)])
+		if debug:
+			var message = "no transition from %s to %s specified"
+			print(message % [str(current_state), str(new_state)])
 		return
 	
 	var object = transitions[current_state][new_state][0]
 	var method = transitions[current_state][new_state][1]
 	
 	if not object.has_method(method):
+		# always complain about this
 		print("%s doesn't have method %s" % [str(object), method])
 		return
 	else:
 		object.call(method)
+		if debug:
+			print("transitioned from %s to %s" % [str(current_state), str(new_state)])
 		current_state = new_state
 
 
